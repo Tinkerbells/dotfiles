9ec44dd83eef3d144b12d6e233d97fba9ac15d73
untermensch <daniel_unter_work@pm.me>
2022-07-15 17:17:47 +0300

    add neovim config

---
+++ neovim/.config/nvim/lua/user/lualine.lua
@@ -0,0 +1,244 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+
+vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#f5a97f", bg = "#24273a" })
+vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#cad3f5", bg = "#24273a", bold = false })
+-- vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D7BA7D", bg = "#252525" })
+vim.api.nvim_set_hl(0, "SLProgress", { fg = "#8087a2", bg = "#24273a" })
+vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#8087a2", bg = "#24273a" })
+vim.api.nvim_set_hl(0, "SLLSP", { fg = "#8aadf4", bg = "#24273a" })
+-- darkerplus
+-- vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#E8AB53", bg = "#303030" })
+-- vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#abb2bf", bg = "#303030", bold = false })
+-- -- vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D7BA7D", bg = "#252525" })
+-- vim.api.nvim_set_hl(0, "SLProgress", { fg = "#abb2bf", bg = "#303030" })
+-- vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#545862", bg = "#252525" })
+local mode_color = {
+  n = "#8aadf4",
+  i = "#a6da95",
+  v = "#f5bde6",
+  [""] = "#ee99a0",
+  V = "#ed8796",
+  -- c = '#B5CEA8',
+  -- c = '#D7BA7D',
+  c = "#56b6c2",
+  no = "#61afef",
+  s = "#d19a66",
+  S = "#d19a66",
+  [""] = "#d19a66",
+  ic = "#e06c75",
+  R = "#d19a66",
+  Rv = "#e06c75",
+  cv = "#61afef",
+  ce = "#61afef",
+  r = "#e06c75",
+  rm = "#56b6c2",
+  ["r?"] = "#56b6c2",
+  ["!"] = "#56b6c2",
+  t = "#98c379",
+}
+
+local mode = {
+  -- mode component
+  function()
+    -- return "▊"
+    return "  "
+    -- return "  "
+  end,
+  color = function()
+    -- auto change color according to neovims mode
+    return { bg = mode_color[vim.fn.mode()] }
+  end,
+  -- padding = { right = 1 },
+  padding = 0,
+}
+
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = " ", warn = " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width,
+  separator = "%#SLSeparator#" .. "│ " .. "%*",
+}
+
+-- local mode = {
+--   "mode",
+--   fmt = function(str)
+--     return "-- " .. str .. " --"
+--   end,
+-- }
+
+local filetype = {
+  "filetype",
+  icons_enabled = true,
+  -- icon = nil,
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
+  -- color = "Constant",
+  colored = false,
+}
+
+local progress = {
+  "progress",
+  color = "SLProgress",
+  -- fmt = function(str)
+  --   print(vim.fn.expand(str))
+  --   if str == "1%" then
+  --     return "TOP"
+  --   end
+  --   if str == "100%" then
+  --     return "BOT"
+  --   end
+  --   return str
+  -- end,
+  -- padding = 0,
+}
+
+-- local progress = {
+--   "progress",
+--   fmt = function(str)
+--     print(vim.fn.expand(str))
+--     if str == "1%" then
+--       return "TOP"
+--     end
+--     if str == "100%" then
+--       return "BOT"
+--     end
+--     return str
+--   end,
+--   -- padding = 0,
+-- }
+
+local current_signature = function()
+  if not pcall(require, "lsp_signature") then
+    return
+  end
+  local sig = require("lsp_signature").status_line(30)
+  -- return sig.label .. "🐼" .. sig.hint
+  return "%#SLSeparator#" .. sig.hint .. "%*"
+end
+
+-- cool function for progress
+-- local progress = function()
+--   local current_line = vim.fn.line "."
+--   local total_lines = vim.fn.line "$"
+--   local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+--   local line_ratio = current_line / total_lines
+--   local index = math.ceil(line_ratio * #chars)
+--   -- return chars[index]
+--   return "%#SLProgress#" .. chars[index] .. "%*"
+-- end
+
+local spaces = {
+  function()
+    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  end,
+  padding = 0,
+  separator = "%#SLSeparator#" .. " │" .. "%*",
+}
+
+local lanuage_server = {
+  function()
+    local clients = vim.lsp.buf_get_clients()
+    local client_names = {}
+
+    -- add client
+    for _, client in pairs(clients) do
+      if client.name ~= "copilot" and client.name ~= "null-ls" then
+        table.insert(client_names, client.name)
+      end
+    end
+
+    local buf_ft = vim.bo.filetype
+
+    -- add formatter
+    local s = require "null-ls.sources"
+    local available_sources = s.get_available(buf_ft)
+    local registered = {}
+    for _, source in ipairs(available_sources) do
+      for method in pairs(source.methods) do
+        registered[method] = registered[method] or {}
+        table.insert(registered[method], source.name)
+      end
+    end
+
+    local formatter = registered["NULL_LS_FORMATTING"]
+    local linter = registered["NULL_LS_DIAGNOSTICS"]
+    if formatter ~= nil then
+      vim.list_extend(client_names, formatter)
+    end
+    if linter ~= nil then
+      vim.list_extend(client_names, linter)
+    end
+
+    -- join client names with commas
+    local client_names_str = table.concat(client_names, ", ")
+    -- 
+    return "%#SLLSP#" .. "[" .. client_names_str .. "]" .. "%*"
+  end,
+  padding = 0,
+  cond = hide_in_width,
+  separator = "%#SLSeparator#" .. " │ " .. "%*",
+}
+
+-- local location = {
+--   "location",
+--   color = function()
+--     -- darkerplus
+--     -- return { fg = "#252525", bg = mode_color[vim.fn.mode()] }
+--     return { fg = "#1E232A", bg = mode_color[vim.fn.mode()] }
+--   end,
+-- }
+
+lualine.setup {
+  options = {
+    globalstatus = true,
+    icons_enabled = true,
+    -- theme = "auto",
+    theme = theme,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { mode, branch },
+    lualine_b = { diagnostics },
+    -- lualine_c = {},
+    lualine_c = { { current_signature, cond = hide_in_width } },
+    -- lualine_x = { diff, spaces, "encoding", filetype },
+    lualine_x = { diff, lanuage_server, spaces, filetype },
+    lualine_y = { progress },
+    -- lualine_z = { location },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
+}
---
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,138 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim"
+  -- use "tiagovla/scope.nvim"
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
+  use "akinsho/toggleterm.nvim"
+  use "christianchiarulli/lualine.nvim"
+  use "ahmedkhalf/project.nvim"
+  use "lewis6991/impatient.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
+  use "kylechui/nvim-surround"
+  use {
+    "abecodes/tabout.nvim",
+    wants = { "nvim-treesitter" }, -- or require if not used so far
+  }
+  use "nacro90/numb.nvim"
+  use "monaqa/dial.nvim"
+  use "norcalli/nvim-colorizer.lua"
+  use "windwp/nvim-spectre"
+  use "kevinhwang91/nvim-bqf"
+  use "ThePrimeagen/harpoon"
+  use "MattesGroeger/vim-bookmarks"
+  -- use "Mephistophiles/surround.nvim"
+  use { "michaelb/sniprun", run = "bash ./install.sh" }
+  use {
+
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    ft = "markdown",
+  }
+  -- use "stevearc/stickybuf.nvim"
+
+  -- UI
+  use "stevearc/dressing.nvim"
+  use "ghillb/cybu.nvim"
+  use { "christianchiarulli/nvim-gps", branch = "text_hl" }
+  use "tversteeg/registers.nvim"
+  use "rcarriga/nvim-notify"
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
+  use "tamago324/lir.nvim"
+  use "folke/which-key.nvim"
+  use "folke/zen-mode.nvim"
+  use "karb94/neoscroll.nvim"
+  use "folke/todo-comments.nvim"
+  use "andymass/vim-matchup"
+
+  -- Colorschemes
+  use "catppuccin/nvim"
+  vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-emoji"
+  use "hrsh7th/cmp-nvim-lua"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "RRethy/vim-illuminate"
+
+  -- Typescript TODO: set this up, also add keybinds to ftplugin
+  use "jose-elias-alvarez/typescript.nvim"
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
+  -- Treesitter
+  use "nvim-treesitter/nvim-treesitter"
+  use "JoosepAlviste/nvim-ts-context-commentstring"
+  use "p00f/nvim-ts-rainbow"
+  use "windwp/nvim-ts-autotag"
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+  use "kdheepak/lazygit.nvim"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
